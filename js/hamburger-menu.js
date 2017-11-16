de323a5cb78fd1fbc9b21384ce650b4bc5bf8893
ericharm <ericharm@gmail.com>
2017-11-16 16:45:40 -0500

    prepare to go live

---
+++ js/hamburger-menu.js
@@ -0,0 +1,23 @@
+setTimeout(function () {
+    var mobileNavOpenBtn = document.getElementById('mobile-nav-open');
+    var mobileNav = document.getElementById('mobile-nav');
+    var toggleMobileNav = function (e) {
+        if (mobileNav.classList.contains('open')) {
+            mobileNav.classList.remove('open');
+            mobileNavOpenBtn.classList.remove('open');
+        } else {
+            mobileNav.classList.add('open');
+            mobileNavOpenBtn.classList.add('open');
+        }
+        e.stopPropagation();
+    };
+    mobileNavOpenBtn.addEventListener('click', toggleMobileNav);
+    mobileNav.addEventListener('click', toggleMobileNav);
+    if (window.location.search !== '') {
+        window.history.replaceState({}, '', window.location.origin);
+    }
+
+    var video = document.getElementById("covervid");
+    if (video) video.play();
+}, 400);
+
