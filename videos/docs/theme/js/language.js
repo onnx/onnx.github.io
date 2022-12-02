2412d2edfb1e0c24b2d26f49f25a902e2a3f3115
Andreas Fehlner <fehlner@arcor.de>
2022-12-02 12:59:56 +0100

    delete docs folder, will be created by github action
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>

--- videos/docs/theme/js/language.js
+++
@@ -1,9 +0,0 @@
-$(window).bind( 'hashchange', function(){
-  "use strict";
-  var hash = location.hash;
-  $('.language').hide();
-  if (hash != "") {
-    $(hash).show();
-  }
-});
-$(window).trigger( 'hashchange' );
