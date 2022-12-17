7f4af342b8b7969c195631578151c9f2b5e2b733
andife <fehlner@arcor.de>
2022-12-17 14:57:20 +0100

    replace
    Signed-off-by: andife <fehlner@arcor.de>

--- srcvideos/afonnxtools/replace2.sh
+++ srcvideos/afonnxtools/replace2.sh
@@ -1,4 +1,4 @@
-find . -type f -name "*.html" -exec sed -i 's+<link href="/theme+<link href="https://andife.github.io/onnxvideo.github.io/videos/theme+g' {} +
+find . -type f -name "*.html" -exec sed -i 's+<link href="/theme+<link href="https://andife.github.io/onnx.github.io/videos/theme+g' {} +
 
 find . -type f -name "*.html" -exec sed -i 's+<a href="/onnx-community-day-2022_06/+<a href="https://andife.github.io/onnx.github.io/videos/onnx-community-day-2022_06/+g' {} +
 
