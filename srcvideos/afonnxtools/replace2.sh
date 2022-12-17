e2e0c283725b3030bdc623e8dd8ba2651cd5065a
andife <fehlner@arcor.de>
2022-12-17 14:50:33 +0100

    fix theme
    Signed-off-by: andife <fehlner@arcor.de>

--- srcvideos/afonnxtools/replace2.sh
+++ srcvideos/afonnxtools/replace2.sh
@@ -1,7 +1,14 @@
-#find . -type f -name "*.html" -exec sed -i 's+"/theme+"https://andife.github.io/onnxvideo.github.io/theme+g' {} +
-
-#find . -type f -name "*.html" -exec sed -i 's+"theme+"https://andife.github.io/onnxvideo.github.io/theme+g' {} +
+find . -type f -name "*.html" -exec sed -i 's+<link href="/theme+<link href="https://andife.github.io/onnxvideo.github.io/videos/theme+g' {} +
 
 find . -type f -name "*.html" -exec sed -i 's+<a href="/onnx-community-day-2022_06/+<a href="https://andife.github.io/onnx.github.io/videos/onnx-community-day-2022_06/+g' {} +
 
+find . -type f -name "*.html" -exec sed -i 's+<a href="/onnx-community-day-2021_10/+<a href="https://andife.github.io/onnx.github.io/videos/onnx-community-day-2021_10/+g' {} +
+
+
 find . -type f -name "*.html" -exec sed -i 's+<a href="/onnx-community-day-2021_03/+<a href="https://andife.github.io/onnx.github.io/videos/onnx-community-day-2021_03/+g' {} +
+
+
+find . -type f -name "*.html" -exec sed -i 's+<a href="/pytorch-devcon-2019/+<a href="https://andife.github.io/onnx.github.io/videos/pytorch-devcon-2019/+g' {} +
+
+
+find . -type f -name "*.html" -exec sed -i 's+<a href="/tvmcon-2021/+<a href="https://andife.github.io/onnx.github.io/videos/tvmcon-2021/+g' {} +
